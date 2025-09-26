865b545b11d9c5c445cb38916f16598016ccd9dc
AM Agcon <160177830+amagcon@users.noreply.github.com>
2025-09-25 22:19:24 -0400

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from supabase import create_client, Client
+
+@st.cache_resource(show_spinner=False)
+def get_supabase() -> Client:
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["anon_key"]
+    return create_client(url, key)
 
