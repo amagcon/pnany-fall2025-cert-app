b0ae3be4598ec2c538606e176fd2a5ba608da4b1
AM Agcon <160177830+amagcon@users.noreply.github.com>
2025-09-27 21:10:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from google.oauth2.service_account import Credentials
 # Streamlit Config
 # =========================
 
-st.image("https://example.com/path/to/logo.png", width=250)
+st.image("https://drive.google.com/file/d/1bNQ52mcvyWl06nUp2QDjnx5q_aYTKxeh/view?usp=drive_link", width=250)
 st.set_page_config(
     page_title="PNANY Fall Conference 2025 — Evaluation, Post-test, & Certificate",
     page_icon="🎓",
